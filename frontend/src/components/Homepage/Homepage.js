53eb037e7c7aa30ad30a2dcfca3f8eeaf21dc11b
Andreea Cioacă <deea.cioaca23@gmail.com>
2022-01-20 22:06:40 +0200

    Homepage
    Homepage

--- frontend/src/components/Homepage/Homepage.js
+++ frontend/src/components/Homepage/Homepage.js
@@ -17,7 +17,27 @@ const Homepage = () => {
 
   return (
     <div className="containerHomepage">
-      {!isHidden ? <h2>Salutare {user.prenume}!</h2> : <span></span>}
+
+      <div className="homepage-left">
+        {!isHidden ? <h1>Salutare, {user.prenume}!</h1> : <span></span>}
+        <br /> 
+        <h3>Teamwork makes the project work!</h3>
+        <p>
+          <br /> 
+          Bine ai venit în locul perfect pentru colaborare și dezvoltare.
+          <br /> 
+          <br /> 
+          Acum poți încărca oricând un proiect sau livra parțial o parte din acesta.
+          <br /> 
+          <br /> 
+          Iar pentru că feedback-ul are un rol important în evaluarea oricărui proiect, acum poți atât să oferi note colegilor, cât și să primești la rândul tău note din partea lor și a profesorilor.
+        </p>
+      </div>
+
+      <div className="homepage-right">
+        <img src="background.png"/>
+      </div>
+
     </div>
   );
 };
